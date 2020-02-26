2507fcac59cb2a71372525da34d50c67253073ca
Kasia <s15804@pjwstk.edu.pl>
2020-02-26 12:25:11 +0100

    lab01 - BMI calculator v1.1

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,18 +1,12 @@
-package com.example.helloworld;
+package com.example.bmicalculator;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.example.bmicalculator.R;
 
 public class MainActivity extends AppCompatActivity {
     EditText waga, wzrost;
@@ -27,18 +21,6 @@ public class MainActivity extends AppCompatActivity {
         waga = findViewById(R.id.waga);
         wzrost = findViewById(R.id.wzrost);
         wynik = findViewById(R.id.wynik);
-
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Hello world!!!", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
     }
 
     public void obliczBMI(View view) {
@@ -72,26 +54,4 @@ public class MainActivity extends AppCompatActivity {
 
         wynik.setText(obliczenia);
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
-//        return super.onOptionsItemSelected(item);
-//    }
 }
